3c12548308e280f0cf1ecf7f56f3b36b33e43287
ayhanpavloliev <120591922+ayhanpavloliev@users.noreply.github.com>
2023-04-26 21:20:23 +0300

    Remove [Required] attribute from User class properties

--- KRCars1298/Data/Models/User.cs
+++ KRCars1298/Data/Models/User.cs
@@ -5,17 +5,14 @@ namespace KRCars1298.Data.Models
 {
     public class User : IdentityUser
     {
-        [Required]
         [StringLength(20, MinimumLength = 2)]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
-
-        [Required]
+        
         [StringLength(20, MinimumLength = 2)]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required]
         [StringLength(50, MinimumLength = 2)]
         public string City { get; set; }
     }
