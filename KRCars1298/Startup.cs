733ed6cbe1613b729f09ffce089e123a9bf89592
ayhanpavloliev <120591922+ayhanpavloliev@users.noreply.github.com>
2023-05-29 20:42:22 +0300

    Change ad details page title

--- KRCars1298/Startup.cs
+++ KRCars1298/Startup.cs
@@ -25,7 +25,6 @@ namespace KRCars1298
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>();
@@ -42,7 +41,6 @@ namespace KRCars1298
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -53,7 +51,7 @@ namespace KRCars1298
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
